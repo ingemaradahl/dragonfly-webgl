52883dfc81264d64a68c276f88b27dd48af0264f
Marcus Lönnberg <marcusl@opera.com>
2012-07-09 11:32:17 +0200

    Trace will now get the captured snapshot without having to change tab. The snapshot dropdown will now automatically change to the captured snapshot. Some cleanup in trace view.

--- src/webgl/trace_view.js
+++ src/webgl/trace_view.js
@@ -33,7 +33,7 @@ cls.WebGLTraceView = function(id, name, container_class)
     //var ctx_id = window['cst-selects']['context-select'].get_selected_context();
     // TODO: FIXME
     var snapshot = window['cst-selects']['snapshot-select'].get_selected_snapshot();
-    
+
     if (snapshot != null)
     {
       var trace = snapshot.trace;
@@ -71,23 +71,6 @@ cls.WebGLTraceView = function(id, name, container_class)
     }
   };
 
-  this._on_new_trace = function(trace)
-  {
-    this._render();
-  };
-
-  this._on_context_change = function(ctx_id)
-  {
-    this._current_context = ctx_id;
-    var trace = window.webgl.data[ctx_id].get_latest_trace(ctx_id);
-    if (trace != null)
-    {
-      this._table.set_data(this._format_trace_table(trace));
-    }
-
-    this._render();
-  };
-
   this._format_trace_table = function(trace)
   {
     var tbl_data = [];
@@ -95,8 +78,8 @@ cls.WebGLTraceView = function(id, name, container_class)
     {
       var call = trace[i];
       var call_text = window.webgl.api.function_call_to_string(call.function_name, call.args);
-      if (trace[i].have_result) call_text += " = " + String(trace[i].result)
-      if (trace[i].have_error) call_text += " -> Error code: " + String(trace[i].error_code)
+      if (trace[i].have_result) call_text += " = " + String(trace[i].result);
+      if (trace[i].have_error) call_text += " -> Error code: " + String(trace[i].error_code);
       tbl_data.push({"number" : String(i + 1), "call" : call_text});
     }
     return tbl_data;
@@ -138,8 +121,6 @@ cls.WebGLTraceView = function(id, name, container_class)
   eh.click["webgl-trace-row"] = this._on_row_click.bind(this);
   eh.click["webgl-trace-argument"] = this._on_argument_click.bind(this);
 
-  messages.addListener('webgl-new-trace', this._on_new_trace.bind(this));
-  messages.addListener('webgl-context-selected', this._on_context_change.bind(this));
   messages.addListener('webgl-changed-snapshot', this._render.bind(this));
 
   this.init(id, name, container_class);
@@ -166,7 +147,7 @@ cls.WebGLTraceView.create_ui_widgets = function()
         class: 'context-select-dropdown',
         template: window['cst-selects']['snapshot-select'].getTemplate()
       }
-    ] 
+    ]
   );
 };
 
