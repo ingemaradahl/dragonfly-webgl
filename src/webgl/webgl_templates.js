0db89edc9d64f12c7589b41f01741f42138f673a
Marcus Lönnberg <marcusl@opera.com>
2012-07-31 10:32:44 +0200

    Added error message dialog with possible causes
    The current error messages are only temporary.
    Also added a temporary solution to show texture calls that does not have
    a matching texture object.

--- src/webgl/webgl_templates.js
+++ src/webgl/webgl_templates.js
@@ -93,7 +93,7 @@ window.templates.webgl.buffer_base = function(buffer, coordinates, selected_item
       ["select",
       buffer_options,
       "handler", "webgl-select-layout",
-      "id", "webgl-layout-selector" 
+      "id", "webgl-layout-selector"
       ],
     ];
 
@@ -128,7 +128,7 @@ window.templates.webgl.buffer_base = function(buffer, coordinates, selected_item
       history,
       coordinate_selector,
       inputbox,
-      data_table 
+      data_table
     ]
   ];
 };
@@ -621,11 +621,56 @@ window.templates.webgl.state_parameters = function(state_parameters)
   ];
 };
 
+window.templates.webgl.error_message = function(call)
+{
+  var error_code = window.webgl.api.constant_value_to_string(call.error_code);
+  var header = [
+    "h2", [
+      ["span", "Error: " + error_code],
+    ]
+  ];
+
+  var content = [header];
+
+  var result = [
+    "div", content,
+    "class", "error-message"
+  ];
+
+  var fun_errors = cls.WebGLAPI.ERROR_MESSAGES[call.function_name];
+  if (!fun_errors || !(error_code in fun_errors)) return result;
+
+  var errors = fun_errors[error_code];
+  errors = errors.map(function(error){return [
+    "div",
+    [
+      [
+        "span", error.txt
+      ],
+      !error.ref ? [] : [
+        "a", "Read more here.",
+        "href", error.ref
+      ]
+    ]
+  ];});
+
+  content.push([
+    "div", "",
+    "class", "divider"
+  ]);
+  content.push([
+    "div", "Possible cause" + (errors.length === 1 ? "" : "s") + ":",
+    "class", "cause"
+  ]);
+  content.push(errors);
+
+  return result;
+};
+
 /**
  * @param {Array} template optional, should contain a html structure of other
  *   content that should be shown below the header.
  */
-
 window.templates.webgl.info_with_header = function(template)
 {
   var header = [
@@ -633,19 +678,19 @@ window.templates.webgl.info_with_header = function(template)
       [
         "h2", [
           ["span", "Start of frame"],
-        ],
+        ]
       ]
     ],
     "class", "draw-call-info"
   ];
 
-    var html = [header];
-    if (template)
-    {
-      html.push(template);
-    }
+  var html = [header];
+  if (template)
+  {
+    html.push(template);
+  }
 
-    return html;
+  return html;
 };
 
 window.templates.webgl.call_with_header = function(call, trace_call, state_parameters, template)
@@ -700,7 +745,13 @@ window.templates.webgl.call_with_header = function(call, trace_call, state_param
   ];
 
   var res = [header];
-  var content = [state];
+
+  var content = [];
+  if (trace_call.error_code !== cls.WebGLAPI.CONSTANTS.NO_ERROR)
+  {
+    content.push(window.templates.webgl.error_message(trace_call));
+  }
+  content.push(state);
   res.push([
     "div",
     content,
