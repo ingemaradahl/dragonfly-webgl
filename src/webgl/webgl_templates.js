05d655e3d20dc8ec5a39751f10463540bf4710f5
Ingemar Ådahl <ingemar.adahl@gmail.com>
2012-08-02 17:46:16 +0200

    Quick fix for ONE and LINES both mapping to the enum 1

--- src/webgl/webgl_templates.js
+++ src/webgl/webgl_templates.js
@@ -286,7 +286,9 @@ window.templates.webgl.buffer_preview = function (buffer_settings)
           ['select',
             buffer_settings.options.modes.map(function(mode) {
               var option = ['option',
-                window.webgl.api.constant_value_to_string(mode),
+                mode === 1 // The api defined in webgl has the enum '1' mapped to both LINES and ONE
+                  ? "LINES"
+                  : window.webgl.api.constant_value_to_string(mode),
                 'value', String(mode)
               ];
 
