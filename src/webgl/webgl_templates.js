6eaac03e21d1452d24094e341cf215ef00c92565
Victor Jansson <vjansson@opera.com>
2012-08-21 11:16:50 +0200

    Added unit to units to bytes size numbers in buffer view.

--- src/webgl/webgl_templates.js
+++ src/webgl/webgl_templates.js
@@ -138,8 +138,8 @@ window.templates.webgl.buffer_info_table = function(buffer)
   var buffer_info = [
     {name: "Target", value: buffer.target_string()},
     {name: "Usage", value: buffer.usage_string()},
-    {name: "Size", value: String(buffer.size)},
-    {name: "Length", value: String(buffer.data_length)}
+    {name: "Bytesize", value: String(buffer.size) + " bytes"},
+    {name: "Length", value: String(buffer.data_length) + " elements"}
   ];
 
   var info_table_rows = buffer_info.map(function(info){
